f4c08ee881bc8bc827ce9bfbd65082bab4f72a2a
Jeffrey Massung <massung@gmail.com>
2015-09-15 23:02:04 -0500

    Updated .gitignore. Updated some formatting.

--- targa.lisp
+++ targa.lisp
@@ -1,6 +1,6 @@
 ;;;; Targa Image Loading for Common Lisp
 ;;;;
-;;;; Copyright (c) 2015 by Jeffrey Massung
+;;;; Copyright (c) by Jeffrey Massung
 ;;;;
 ;;;; This file is provided to you under the Apache License,
 ;;;; Version 2.0 (the "License"); you may not use this file
@@ -88,7 +88,9 @@
 
 (in-package :targa)
 
-(eval-when (:execute :compile-toplevel :load-toplevel)
+;;; ----------------------------------------------------
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
   (proclaim '(optimize (speed 3) (debug 0))))
 
 ;;; -----------------------------------------------------
@@ -102,19 +104,27 @@
   (extension      nil :read-only t)
   (developer-tags nil :read-only t))
 
+;;; ----------------------------------------------------
+
 (defmethod print-object ((tga tga) stream)
   "Output a parsed targa to a stream."
   (print-unreadable-object (tga stream :type t)
-    (with-slots (image-type (w width) (h height) (bpp pixel-size))
+    (with-slots (image-type width height pixel-size)
         (tga-header tga)
-      (format stream "~a bit, ~ax~a, ~a" bpp w h (case image-type
-                                                   (#b0000 :no-image)
-                                                   (#b0001 :color-mapped-image)
-                                                   (#b0010 :true-color-image)
-                                                   (#b0011 :black-and-white-image)
-                                                   (#b1001 :rle-color-mapped-image)
-                                                   (#b1010 :rle-true-color-image)
-                                                   (#b1011 :rle-black-and-white-image))))))
+      (format stream "~a bit, ~ax~a, ~a"
+              pixel-size
+              width
+              height
+              (case image-type
+                (#b0000 :no-image)
+                (#b0001 :color-mapped-image)
+                (#b0010 :true-color-image)
+                (#b0011 :black-and-white-image)
+                (#b1001 :rle-color-mapped-image)
+                (#b1010 :rle-true-color-image)
+                (#b1011 :rle-black-and-white-image))))))
+
+;;; ----------------------------------------------------
 
 (defun tga-get-pixel (tga x y)
   "Lookup a pixel in the image based on the screen origin of the targa."
@@ -153,6 +163,8 @@
   (pixel-size            nil :read-only t)
   (image-descriptor      nil :read-only t))
 
+;;; ----------------------------------------------------
+
 (defun read-tga-header (stream)
   "Read a targa header from an input stream."
   (make-tga-header
@@ -169,21 +181,31 @@
    :pixel-size (read-byte stream)
    :image-descriptor (read-byte stream)))
 
+;;; ----------------------------------------------------
+
 (defun tga-header-color-map-p (h)
   "T if the image has a color map."
   (plusp (tga-header-color-map-type h)))
+
+;;; ----------------------------------------------------
 
 (defun tga-header-rle-compressed-p (h)
   "T if the image is run-length-encoded."
   (plusp (logand (tga-header-image-type h) #b1000)))
 
+;;; ----------------------------------------------------
+
 (defun tga-header-attribute-bits (h)
   "Return the number of attribute bits per pixel."
   (logand (tga-header-image-descriptor h) #b1111))
 
+;;; ----------------------------------------------------
+
 (defun tga-header-screen-x-origin (h)
   "Return the origin of each scanline - :left or :right."
   (if (zerop (logand (tga-header-image-descriptor h) #b010000)) :left :right))
+
+;;; ----------------------------------------------------
 
 (defun tga-header-screen-y-origin (h)
   "Return the origin of the image - :bottom or :top."
@@ -210,6 +232,8 @@
   (scan-line-offset        nil :read-only t)
   (attributes-type         nil :read-only t))
 
+;;; ----------------------------------------------------
+
 (defun read-tga-extension (stream offset)
   "Parse an extension from an input stream at a given offset."
   (when (file-position stream offset)
@@ -233,6 +257,8 @@
      :scan-line-offset (read-long stream)
      :attributes-type (read-byte stream))))
 
+;;; ----------------------------------------------------
+
 (defun tga-read-color-correction-table (stream tga)
   "Read the color-correction table."
   (with-slots (extension)
@@ -240,22 +266,25 @@
     (when extension
       (let ((offset (tga-extension-color-correction-offset extension)))
         (when (and (plusp offset) (file-position stream offset))
-          (loop with table = (make-array 256)
+          (loop
+             with table = (make-array 256)
 
-                ;; loop over the entire table
-                for i below 256
+             ;; loop over the entire table
+             for i below 256
 
-                ;; read each color component
-                for alpha = (float (/ (read-short stream) 65535))
-                for red = (float (/ (read-short stream) 65535))
-                for green = (float (/ (read-short stream) 65535))
-                for blue = (float (/ (read-short stream) 65535))
+             ;; read each color component
+             for alpha = (float (/ (read-short stream) 65535))
+             for red = (float (/ (read-short stream) 65535))
+             for green = (float (/ (read-short stream) 65535))
+             for blue = (float (/ (read-short stream) 65535))
 
-                ;; fill in the table
-                do (setf (aref table i) (list red green blue alpha))
+             ;; fill in the table
+             do (setf (aref table i) (list red green blue alpha))
 
-                ;; done
-                finally (return table)))))))
+             ;; done
+             finally (return table)))))))
+
+;;; ----------------------------------------------------
 
 (defun tga-read-scanline-table (stream tga)
   "Read all the scanline offsets."
@@ -264,15 +293,18 @@
     (when extension
       (let ((offset (tga-extension-scan-line-offset extension)))
         (when (and (plusp offset) (file-position stream offset))
-          (loop with height = (tga-header-height header)
-                with scanlines = (make-array height)
-                
-                ;; loop over all the scanlines and read their offset
-                for i below height
-                do (setf (aref scanlines i) (read-long stream))
-                
-                ;; return the array of offsets
-                finally (return scanlines)))))))
+          (loop
+             with height = (tga-header-height header)
+             with scanlines = (make-array height)
+
+             ;; loop over all the scanlines and read their offset
+             for i below height
+             do (setf (aref scanlines i) (read-long stream))
+
+             ;; return the array of offsets
+             finally (return scanlines)))))))
+
+;;; ----------------------------------------------------
 
 (defun tga-read-postage-stamp (stream tga)
   "Read the postage stamp image from the targa stream."
@@ -281,28 +313,30 @@
     (when extension
       (let ((offset (tga-extension-postage-stamp-offset extension)))
         (when (and (plusp offset) (file-position stream offset))
-          (loop with width = (read-byte stream)
-                with height = (read-byte stream)
+          (loop
+             with width = (read-byte stream)
+             with height = (read-byte stream)
 
-                ;; allocate a new array of pixels
-                with pixels = (make-array (* width height) :fill-pointer 0)
+             ;; allocate a new array of pixels
+             with pixels = (make-array (* width height) :fill-pointer 0)
 
-                ;; create a copy of the tga structure
-                with postage = (make-tga :header (copy-tga-header header)
-                                         :color-map color-map
-                                         :pixels pixels)
+             ;; create a copy of the tga structure
+             with postage = (make-tga :header (copy-tga-header header)
+                                      :color-map color-map
+                                      :pixels pixels)
 
-                ;; loop over each scanline in the postage stamp
-                for y below height
+             ;; loop over each scanline in the postage stamp
+             for y below height
 
-                ;; read every pixel in the scanline into an array
-                do (dotimes (x width)
-                     (vector-push (read-pixel stream header color-map) pixels))
+             ;; read every pixel in the scanline into an array
+             do (dotimes (x width)
+                  (vector-push (read-pixel stream header color-map) pixels))
 
-                ;; return the new tga image after updating the width and height
-                finally (return (prog1 postage
-                                  (setf (slot-value (tga-header postage) 'width) width
-                                        (slot-value (tga-header postage) 'height) height)))))))))
+             ;; return the new tga image after updating the width and height
+             finally (return (prog1 postage
+                               (with-slots ((w width) (h height))
+                                   (tga-header postage)
+                                 (setf w width h height))))))))))
 
 ;;; -----------------------------------------------------
 
@@ -313,31 +347,36 @@
   (size   nil :read-only t)
   (bytes  nil :read-only t))
 
+;;; -----------------------------------------------------
+
 (defmethod print-object ((tag tga-developer-tag) stream)
   "Output a developer tag to a stream."
   (print-unreadable-object (tag stream :type t)
     (format stream "ID ~a" (tga-developer-tag-id tag))))
 
+;;; -----------------------------------------------------
+
 (defun read-tga-developer-tags (stream offset)
   "Read all the developer tags from an input targa stream."
   (when (file-position stream offset)
-    (loop for i below (read-short stream)
+    (loop
+       for i below (read-short stream)
 
-          ;; read the initial values for each tag
-          for id = (read-short stream)
-          for offset = (read-long stream)
-          for size = (read-long stream)
-          
-          ;; read each tag
-          collect (make-tga-developer-tag :id id :offset offset :size size)
-          into tags
-          
-          ;; read the byte data for each tag and return all the tags
-          finally (dolist (tag tags tags)
-                    (with-slots (offset size bytes)
-                        tag
-                      (when (file-position stream offset)
-                        (setf bytes (read-bytes stream size))))))))
+       ;; read the initial values for each tag
+       for id = (read-short stream)
+       for offset = (read-long stream)
+       for size = (read-long stream)
+
+       ;; read each tag
+       collect (make-tga-developer-tag :id id :offset offset :size size)
+       into tags
+
+       ;; read the byte data for each tag and return all the tags
+       finally (dolist (tag tags tags)
+                 (with-slots (offset size bytes)
+                     tag
+                   (when (file-position stream offset)
+                     (setf bytes (read-bytes stream size))))))))
 
 ;;; -----------------------------------------------------
 
@@ -347,17 +386,23 @@
   (declare (optimize (speed 3) (debug 0)))
   (the fixnum (logior (read-byte s) (ash (read-byte s) 8))))
 
+;;; -----------------------------------------------------
+
 (declaim (inline read-long))
 (defun read-long (s)
   "Read an unsigned, little-endian, 32-bit long."
   (declare (optimize (speed 3) (debug 0)))
   (the fixnum (logior (read-short s) (ash (read-short s) 16))))
 
+;;; -----------------------------------------------------
+
 (defun read-bytes (s len)
   "Read a sequence of bytes from the stream."
   (let ((bytes (make-array len :element-type '(unsigned-byte 8))))
     (prog1 bytes
       (read-sequence bytes s))))
+
+;;; -----------------------------------------------------
 
 (defun read-string (s len)
   "Read a null-terminated string of a given length."
@@ -367,23 +412,31 @@
                                    bytes
                                  (subseq bytes 0 i))))))
 
+;;; -----------------------------------------------------
+
 (defun read-date/time (s)
   "Reads three (3) shorts from the stream."
   (list (read-short s)
         (read-short s)
         (read-short s)))
 
+;;; -----------------------------------------------------
+
 (defun read-version (s)
   "Reads a 2-byte version and 1-byte character from the stream."
   (list (read-short s) (code-char (read-byte s))))
+
+;;; -----------------------------------------------------
 
 (defun read-image-id (s h)
   "Read the tga image id as a string from the stream."
   (read-string s (tga-header-id-length h)))
 
+;;; -----------------------------------------------------
+
 (defun read-color (s pixel-size alpha-size)
-  "Read a true color (or black and white) entry from either the color map or the image data."
-  (declare (optimize speed (safety 0) (debug 0) (compilation-speed 0)))
+  "Read a color entry from either the color map or the image data."
+  (declare (optimize (speed 3) (safety 0) (debug 0) (compilation-speed 0)))
   (macrolet ((color-shift (x shift)
                (let ((c (gensym)))
                  `(let ((,c ,x))
@@ -406,42 +459,55 @@
           (let ((x (color-shift (ldb (byte channel-bits 0) c) (- 8 channel-bits))))
             (list x x x (if (zerop alpha-size)
                             255
-                          (color-shift (ldb (byte alpha-size (- pixel-size alpha-size)) c) alpha-shift))))
-        (list (color-shift (ldb (byte channel-size (* channel-size 2)) c) channel-shift) ;r
-              (color-shift (ldb (byte channel-size (* channel-size 1)) c) channel-shift) ;g
-              (color-shift (ldb (byte channel-size (* channel-size 0)) c) channel-shift) ;b
-              (if (zerop alpha-size)
-                  255
-                (color-shift (ldb (byte alpha-size channel-bits) c) alpha-shift)))))))
+                          (let ((alpha (byte alpha-size (- pixel-size alpha-size))))
+                            (color-shift (ldb alpha c) alpha-shift)))))
+        (let ((r-byte (byte channel-size (* channel-size 2)))
+              (g-byte (byte channel-size (* channel-size 1)))
+              (b-byte (byte channel-size (* channel-size 0))))
+          (list (color-shift (ldb r-byte c) channel-shift)
+                (color-shift (ldb g-byte c) channel-shift)
+                (color-shift (ldb b-byte c) channel-shift)
+                (if (zerop alpha-size)
+                    255
+                  (let ((a-byte (byte alpha-size channel-bits)))
+                    (color-shift (ldb a-byte c) alpha-shift)))))))))
+
+;;; -----------------------------------------------------
 
 (defun read-color-map (s h)
   "Read all the color map entries from the stream."
   (when (tga-header-color-map-p h)
-    (loop with length = (tga-header-color-map-length h)
-          with color-map = (make-array length :fill-pointer 0)
+    (loop
+       with length = (tga-header-color-map-length h)
+       with color-map = (make-array length :fill-pointer 0)
 
-          ;; get the size of each color entry
-          with color-size = (tga-header-color-map-entry-size h)
-          with alpha-size = (tga-header-attribute-bits h)
+       ;; get the size of each color entry
+       with color-size = (tga-header-color-map-entry-size h)
+       with alpha-size = (tga-header-attribute-bits h)
 
-          ;; loop over the entire color map
-          for n below length
+       ;; loop over the entire color map
+       for n below length
 
-          ;; read each color into the color map
-          do (vector-push (read-color s color-size alpha-size) color-map)
+       ;; read each color into the color map
+       do (vector-push (read-color s color-size alpha-size) color-map)
 
-          ;; done
-          finally (return color-map))))
+       ;; done
+       finally (return color-map))))
+
+;;; -----------------------------------------------------
 
 (defun read-pixel (s h color-map)
   "Reads a pixel value from the image data in a stream."
   (let ((pixel-size (tga-header-pixel-size h)))
     (if color-map
-        (loop for i below pixel-size by 8
-              sum (ash (read-byte s) i)
-              into pixel
-              finally (return (aref color-map pixel)))
+        (loop
+           for i below pixel-size by 8
+           sum (ash (read-byte s) i)
+           into pixel
+           finally (return (aref color-map pixel)))
       (read-color s pixel-size (tga-header-attribute-bits h)))))
+
+;;; -----------------------------------------------------
 
 (defun read-rle-pixels (s h into color-map)
   "Reads a run-length encoded list of pixels."
@@ -454,28 +520,37 @@
         (dotimes (i len len)
           (vector-push pixel into))))))
 
+;;; -----------------------------------------------------
+
 (defun read-scanline (s h into color-map)
   "Read a y-axis scanline of pixels."
-  (loop with rle-p = (tga-header-rle-compressed-p h)
-        with pixels-left = (tga-header-width h)
+  (loop
+     with rle-p = (tga-header-rle-compressed-p h)
+     with pixels-left = (tga-header-width h)
 
-        ;; once all pixels for the scanline have been read, stop
-        until (zerop pixels-left)
+     ;; once all pixels for the scanline have been read, stop
+     until (zerop pixels-left)
 
-        ;; read a single pixel or an rle-compressed list of pixels
-        do (decf pixels-left (if rle-p
-                                 (read-rle-pixels s h into color-map)
-                               (prog1 1
-                                 (vector-push (read-pixel s h color-map) into))))))
+     ;; read a single pixel or an rle-compressed list of pixels
+     do (decf pixels-left (if rle-p
+                              (read-rle-pixels s h into color-map)
+                            (prog1 1
+                              (vector-push (read-pixel s h color-map) into))))))
+
+;;; -----------------------------------------------------
 
 (defun read-image-data (s h color-map)
   "Read all the pixels for the image."
   (when (plusp (tga-header-image-type h))
-    (let ((image (make-array (* (tga-header-width h) (tga-header-height h)) :fill-pointer 0)))
+    (let ((image (make-array (* (tga-header-width h)
+                                (tga-header-height h))
+                             :fill-pointer 0)))
       (dotimes (y (tga-header-height h) image)
         (if (tga-header-color-map-p h)
             (read-scanline s h image color-map)
           (read-scanline s h image nil))))))
+
+;;; -----------------------------------------------------
 
 (defun read-ext-and-tags (s)
   "Determine if this is a truevision file with an extension and a developer tags."
@@ -492,6 +567,8 @@
                   (read-tga-extension s ext-offset))
                 (when (plusp dev-offset)
                   (read-tga-developer-tags s dev-offset)))))))
+
+;;; -----------------------------------------------------
 
 (defun tga-read (s &key (read-ext-and-tags t))
   "Read a tga from an input-bit-stream."
@@ -513,6 +590,8 @@
        :pixels pixels
        :extension ext
        :developer-tags tags))))
+
+;;; -----------------------------------------------------
 
 (defun tga-load (pathname &key (read-ext-and-tags t))
   "Open a file and read the targa image inside."
